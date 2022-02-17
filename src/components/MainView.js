652614f41b82b0a938ee8df6ca633c83b556a556
peltolma <peltolma@student.jyu.fi>
2022-02-17 19:02:11 +0200

    Kategorioiden selailu toimii

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,8 +1,11 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef} from 'react';
 import { AppContext } from '../helpers/Context';
 import "../App.css";
 
 export default function MainView() {
+
+    const ref = useRef(null);
+
     const {setAppState} = useContext(AppContext)
 
     const Navbar = () => {
@@ -11,18 +14,18 @@ export default function MainView() {
                 <ul id='topleft'>
                     <li><h1 id='logo'>NETFLIX</h1></li>
                     <li className='navlink'><a>Etusivu</a></li>
-                    <li className='navlink'><a href='#series'>Sarjat</a></li>
-                    <li className='navlink'><a href='#movies'>Elokuvat</a></li>
-                    <li className='navlink'><a href='#new'>Uutta ja suosittua</a></li>
-                    <li className='navlink'><a href='#list'>Oma lista</a></li>
+                    <li className='navlink'><a href='#'>Sarjat</a></li>
+                    <li className='navlink'><a href='#'>Elokuvat</a></li>
+                    <li className='navlink'><a href='#'>Uutta ja suosittua</a></li>
+                    <li className='navlink'><a href='#'>Oma lista</a></li>
                     <div id='dropdown'>
                         <button id="dropbtn">Selaa</button>
                         <div id="dropdown-content">
                             <a>Etusivu</a>
                             <a href="#">Sarjat</a>
-                            <a href='#movies'>Elokuvat</a>
-                            <a href='#new'>Uutta ja suosittua</a>
-                            <a href='#list'>Oma lista</a>
+                            <a href='#'>Elokuvat</a>
+                            <a href='#'>Uutta ja suosittua</a>
+                            <a href='#'>Oma lista</a>
                         </div>
                     </div>
                 </ul>
@@ -50,31 +53,39 @@ export default function MainView() {
         );
     }
 
+    const scroll = (scrollOffset, categ) => {
+        if((ref.current+scrollOffset)>=0) ref.current = ref.current+scrollOffset;
+        document.getElementById(categ).scrollLeft = ref.current+scrollOffset;
+    }
+
+
     const Category = (props) => {
+        const titleids = [props.name+1, props.name+2, props.name+3, props.name+4, props.name+5, props.name+6, props.name+7, props.name+8, props.name+9, props.name+10]
+
         return (
             <>
                 <h3 className='category' id='movies'>{props.name}</h3>
-                <div className='titles'>
+                <div className='titles' id={'scroll'+props.name}>
                     <div className='arrows'>
-                        <div className='leftarrow'>
+                        <div className='leftarrow' id={props.left} onClick={() => scroll(-40, 'scroll'+props.name)}>
                             <div className='arr' id="leftarrtop"></div>
                             <div className='arr' id='leftarrbtm'></div>
                         </div>
-                        <div className='rightarrow'>
+                        <div className='rightarrow' id={props.right} onClick={() => scroll(40, 'scroll'+props.name)}>
                             <div className='arr' id="rightarrtop"></div>
                             <div className='arr' id='rightarrbtm'></div>
                         </div>
                     </div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
-                    <div className='title'></div>
+                    <div className='title' id={titleids[0]}></div>
+                    <div className='title' id={titleids[1]}></div>
+                    <div className='title' id={titleids[2]}></div>
+                    <div className='title' id={titleids[3]}></div>
+                    <div className='title' id={titleids[4]}></div>
+                    <div className='title' id={titleids[5]}></div>
+                    <div className='title' id={titleids[6]}></div>
+                    <div className='title' id={titleids[7]}></div>
+                    <div className='title' id={titleids[8]}></div>
+                    <div className='title' id={titleids[9]}></div>
                 </div>
             </>
         );
@@ -89,10 +100,10 @@ export default function MainView() {
                 <button id='info'>Lisätietoja</button>
             </div>
             <div id='categories'>
-                <Category name='Sarjat'/>
-                <Category name='Elokuvat'/>
-                <Category name='Uutta ja suosittua'/>
-                <Category name='Oma lista'/>
+                <Category name='Sarjat' left='seriesleft' right='seriesright'/>
+                <Category name='Elokuvat' left='moviesleft' right='moviesright'/>
+                <Category name='Uutta ja suosittua' left='newleft' right='newright'/>
+                <Category name='Oma lista' left='mylistleft' right='mylistright'/>
             </div>
         </div>
     );
